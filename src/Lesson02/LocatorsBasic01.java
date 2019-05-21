5cf3f0eaab29038f6ed69167c90f1d2085ced1d6
shay123g <49873692+shay123g@users.noreply.github.com>
2019-05-21 19:02:10 +0300

    ++

--- src/Lesson02/LocatorsBasic01.java
+++ src/Lesson02/LocatorsBasic01.java
@@ -2,14 +2,18 @@ package Lesson02;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import java.util.List;
 
 public class LocatorsBasic01
 {
-    WebDriver driver;
+    private WebDriver driver;
+
     @BeforeClass
     public void InitVariables()
     {
@@ -23,12 +27,27 @@ public class LocatorsBasic01
     @Test
     public void IdentifyElememt()
     {
+        int SeleniumCount=0,seleniumCount=0;
         driver.get("http://www.seleniumhq.org");
+        List<WebElement> allATags=driver.findElements(By.tagName("a"));
         System.out.println("Element By ID: "+driver.findElement(By.id("menu_about")).getText());
-        System.out.println("Element By Partial Link text: "+driver.findElement(By.partialLinkText("About")).getText());
-
-        //driver.quit();
-
+        System.out.println("Element By Link Text: "+driver.findElement(By.linkText("About")).getText());
+        System.out.println("Element By Partial Link text: "+driver.findElement(By.partialLinkText("Abo")).getText());
+        System.out.println("Total number of links on page: "+allATags.size());
+        for (WebElement elem : allATags)
+        {
+            if (elem.getText().contains("Selenium"))
+                SeleniumCount++;
+            else
+                if (elem.getText().contains("selenium"))
+                    seleniumCount++;
+        }
+        System.out.println("Total number of links with Selenium: "+SeleniumCount);
+        System.out.println("Total number of links with selenium: "+seleniumCount);
     }
-
+    @AfterClass
+    public void Exit()
+    {
+        driver.quit();
+    }
 }
