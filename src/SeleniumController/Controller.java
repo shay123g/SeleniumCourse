3a2176c84f3d27d3dca8228803652692a3294875
shay123g <49873692+shay123g@users.noreply.github.com>
2019-05-28 20:04:03 +0300

    ++

--- src/SeleniumController/Controller.java
+++ src/SeleniumController/Controller.java
@@ -2,6 +2,7 @@ package SeleniumController;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
@@ -9,9 +10,14 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Controller
 {
   WebDriver driver;
+  WebElement Picker;
+  List<WebElement> cells;
   Select selection;
     @BeforeClass
     public void InitVariables()
@@ -22,24 +28,40 @@ public class Controller
         ChromeOptions options = new ChromeOptions();
         options.addArguments("disable-infobars");
         driver = new ChromeDriver(options);
-         Select selection;
+        cells=new ArrayList();
+
+
     }
         @Test
-        public void Test01()
+        public void Test01FillAndSend()
         {
             driver.get("http://atidcollege.co.il/Xamples/ex_controllers.html");
             driver.findElement(By.name("firstname")).sendKeys("Shay");
             driver.findElement(By.name("lastname")).sendKeys("Gazit");
             selection = new Select(driver.findElement(By.id("continents")));
             selection.selectByVisibleText("Africa");
+            driver.findElement(By.id("sex-0")).click();
+            driver.findElement(By.id("exp-3")).click();
+            driver.findElement(By.id("profession-0")).click();
+            driver.findElement(By.id("tool-1")).click();
+            selection = new Select(driver.findElement(By.xpath("//select[@id='selenium_commands']")));
+            selection.selectByIndex(1);
+            driver.findElement(By.name("photo")).sendKeys("C://pic.bmp");
+            driver.findElement(By.id("datepicker")).click();
+            Picker = driver.findElement(By.id("ui-datepicker-div"));
+            cells = Picker.findElements(By.tagName("td"));
+            for (WebElement cell : cells)
+                if (cell.getText().equals("14"))
+                    cell.click();
             driver.findElement(By.id("submit")).click();
+            System.out.println(driver.getCurrentUrl());
             if (driver.getCurrentUrl().contains("Shay") && driver.getCurrentUrl().contains("Gazit"))
                 System.out.println("Test Passed");
             else
                 System.out.println("Test failed");
         }
             @AfterClass
-            public void Exit()
+            public void Test02Exit()
             {
                 driver.quit();
             }
