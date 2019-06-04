a9f5b49b86d1aec2d230b57fc54c8b124ff5dafd
shay123g <49873692+shay123g@users.noreply.github.com>
2019-06-04 18:24:03 +0300

    ++

--- src/InterviewQuestion01/support.java
+++ src/InterviewQuestion01/support.java
@@ -15,13 +15,15 @@ public class support
         //get the first un-ordered list (this is the required list)
         list = driver.findElement(By.className("ms-list")).findElements(By.tagName("span"));
         //count=counter for construct string text: Elem 3, Elem 4...Elem N   count=3,4,5....N
-        int count = 3;
+        int count = 0;
+        //flag=0 - element found. flag=1 element not found
         int flag=0;
         //iterate until end of the list
-        while (count <= list.size())
+        while (count < list.size())
          {
-                String text = "elem " + count;
-                for (int j=2;j<list.size();j++)
+                int elemnum=3;
+                String text = "elem " + elemnum;
+                for (int j=0;j<list.size();j++)
                 {
                     String Actual=list.get(j).getText();
                     if (!(Actual.equals(text)))
@@ -29,11 +31,13 @@ public class support
                     else
                     {
                         flag=0;
+                        elemnum++;
                         count++;
                         break;
                     }
                 }
                 assertEquals(flag,0,text+" didnt exist in list");
+
          }
        }
     }
